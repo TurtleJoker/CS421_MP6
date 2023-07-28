1e9f6cf8e88129a0c69a7f606855a9f99f154a19
Joker Zhong <jiahaozhongjoker@gmail.com>
2023-07-27 23:01:14 -0700

    Update Infer.hs

--- mps/mp6-type-infer/src/Infer.hs
+++ mps/mp6-type-infer/src/Infer.hs
@@ -10,9 +10,9 @@ import Data.Map.Strict as H (Map, insert, lookup, empty, fromList, singleton)
 
 freshInst :: PolyTy -> Infer MonoTy
 freshInst (Forall qVars tau) = do
-    freshVars <- mapM (const freshTau) qVars
-    let subst = zip qVars freshVars
-    return $ applySubst subst tau
+  freshVars <- mapM (const freshTau) qVars
+  let subst = H.fromList $ zip qVars freshVars
+  return $ apply subst tau
 
   {- question 2: occurs check -}
 
@@ -22,83 +22,83 @@ occurs i tau = i `elem` freeVars tau
   {- question 3: unification -}
 
 unify :: [Constraint] -> Infer Substitution
-unify [] = return nullSubst
-unify ((s, t) : cs)
+unify [] = return substEmpty
+unify ((s :~: t) : cs) 
   | s == t = unify cs
-  | isVar s && not (isVar t) = unify ((t, s) : cs)
-  | isVar s && s `occurs` t = throwError (InfiniteType s t)
-  | isVar s && not (s `occurs` t) = do
-      subst <- unify (applySubst [(s, t)] cs)
-      return (composeSubst [(s, applySubst subst t)] subst)
-  | isConstructor s && isConstructor t =
-      if getConstructor s == getConstructor t
-      then unify (zip (getArgs s) (getArgs t) ++ cs)
-      else throwError (CantMatch s t)
-  | otherwise = throwError (CantMatch s t)
+  | TyVar i <- s, not (i `occurs` t) = do
+      let subst = substInit i t
+      cs' <- apply subst cs
+      subst' <- unify cs'
+      return (substCompose subst' subst)
+  | TyVar i <- t = unify ((t :~: s) : cs)
+  | TyConst c1 args1 <- s, TyConst c2 args2 <- t, c1 == c2, length args1 == length args2 =
+      unify ((zipWith (:~:) args1 args2) ++ cs)
+  | otherwise = throwError (Can'tMatch s t)
 
   {- question 4: type inference -}
 
 infer :: TypeEnv -> Exp -> Infer MonoTy
-infer env (Const c) = 
-    let ty = freshInst $ constTySig c 
-    in return ty
+infer env (ConstExp c) = freshInst (constTySig c)
 
-infer env (Var x) = 
-    case H.lookup x env of
-        Nothing -> throwError $ LookupError x
-        Just sch -> freshInst sch
+infer env (VarExp x) = case H.lookup x env of
+  Nothing -> throwError (LookupError x)
+  Just s -> freshInst s
 
 infer env (LetInExp x e1 e2) = do
-    (t1, cs1) <- listen $ infer env e1
-    let sub = unify cs1
-    let env' = H.insert x (GEN env sub t1) env
-    (t, cs) <- listen $ infer env' e2
-    return t `constrainedBy` (cs1 `union` cs)
+  (tau1, constraints1) <- listen $ infer env e1
+  substitution <- unify constraints1
+  let tau1' = apply substitution tau1
+  let env' = H.insert x (quantifyMonoTy tau1') env
+  (tau2, constraints2) <- listen $ infer env' e2
+  tell constraints2
+  return tau2
+
+infer env (BinopExp op e1 e2) = do
+  tau1 <- infer env e1
+  tau2 <- infer env e2
+  tau <- freshTau
+  let tySig = freshInst (binopTySig op)
+  constrain tySig (TyConst "->" [TyConst "->" [tau1, tau2], tau])
+  return tau
+
+infer env (MonopExp op e1) = do
+  tau1 <- infer env e1
+  tau <- freshTau
+  let tySig = freshInst (monopTySig op)
+  constrain tySig (TyConst "->" [tau1, tau])
+  return tau
+
+infer env (IfExp e1 e2 e3) = do
+  tau1 <- infer env e1
+  tau2 <- infer env e2
+  tau3 <- infer env e3
+  constrain tau1 (TyConst "bool" [])
+  constrain tau2 tau3
+  return tau2
 
 infer env (FunExp x e) = do
-    tv <- fresh
-    let env' = H.insert x (Forall [] tv) env
-    t2 <- infer env' e
-    return (tv `fn` t2)
+  tau1 <- freshTau
+  let env' = H.insert x tau1 env
+  tau2 <- infer env' e
+  return (TyConst "->" [tau1, tau2])
 
-infer env (AppExp f e) = do
-    tf <- infer env f
-    te <- infer env e
-    tv <- fresh
-    constrain $ tf ~: (te `fn` tv)
-    return tv
+infer env (AppExp e1 e2) = do
+  tau1 <- infer env e1
+  tau2 <- infer env e2
+  tau <- freshTau
+  constrain tau1 (TyConst "->" [tau2, tau])
+  return tau
 
-infer env (IfThenElseExp e1 e2 e3) = do
-    t1 <- infer env e1
-    t2 <- infer env e2
-    t3 <- infer env e3
-    constrain $ t1 ~: boolTy
-    constrain $ t2 ~: t3
-    return t2
-
-infer env (BinOpExp op e1 e2) = do
-    t1 <- infer env e1
-    t2 <- infer env e2
-    let opTy = freshInst $ binopTySig op
-    constrain $ opTy ~: (t1 `fn` t2 `fn` fresh)
-    return fresh
-
-infer env (MonOpExp op e) = do
-    t <- infer env e
-    let opTy = freshInst $ monopTySig op
-    constrain $ opTy ~: (t `fn` fresh)
-    return fresh
-
-infer env (LetRecInExp f x e1 e2) = do
-    tv1 <- fresh
-    tv2 <- fresh
-    let env' = H.insert x (Forall [] tv1) 
-             $ H.insert f (Forall [] (tv1 `fn` tv2)) env
-    t1 <- infer env' e1
-    constrain $ tv2 ~: t1
-    let env'' = H.insert f (GEN env $ unify $ tv2 ~: t1) env
-    t2 <- infer env'' e2
-    return t2
+infer env (LetRecExp f x e1 e2) = do
+  tau1 <- freshTau
+  tau2 <- freshTau
+  let env' = H.insert x tau1 . H.insert f (TyConst "->" [tau1, tau2]) $ env
+  (tau3, constraints1) <- listen $ infer env' e1
+  substitution <- unify ((tau2 :~: tau3) : constraints1)
+  let env'' = H.insert f (quantifyMonoTy $ apply substitution (TyConst "->" [tau1, tau2])) env
+  (tau, constraints2) <- listen $ infer env'' e2
+  tell constraints2
+  return tau
 
 inferInit :: TypeEnv -> Exp -> Infer PolyTy
 inferInit env e = do
