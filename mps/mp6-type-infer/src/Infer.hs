cb1d65de5983c6cd0f116e4bbeef9ebc843a118d
Joker Zhong <jiahaozhongjoker@gmail.com>
2023-07-30 03:11:57 -0700

    Update Infer.hs

--- mps/mp6-type-infer/src/Infer.hs
+++ mps/mp6-type-infer/src/Infer.hs
@@ -103,13 +103,12 @@ infer env (AppExp f e) = do
 infer env (LetRecExp f x e1 e2) = do
   tau1 <- freshTau
   tau2 <- freshTau
-  let funType = funTy tau1 tau2
-      env' = H.insert x (Forall [] tau1) (H.insert f (Forall [] funType) env)
-  (tau3, constraints) <- listen $ infer env' e1
-  constraints' <- constrain tau2 tau3
-  sub <- unify (constraints' ++ constraints)
-  let generalizedType = gen (apply sub env) (apply sub funType)
-  tau <- infer (H.insert f generalizedType env) e2
+  let genFunType = gen env (funTy tau1 tau2)
+  tau3 <- infer (H.insert x (Forall [] tau1) (H.insert f genFunType env)) e1
+  (_, constraints) <- listen $ constrain tau2 tau3
+  sub <- unify (constraints ++ [tau2 :~: tau3])
+  let genType = gen (apply sub env) (apply sub (funTy tau1 tau2))
+  tau <- infer (H.insert f genType env) e2
   return tau
 
 inferInit :: TypeEnv -> Exp -> Infer PolyTy
